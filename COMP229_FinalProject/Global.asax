fb7393ddcbc786cac6e650ba7fd3225daa2f6ab1
Logan Junhwi Kim <35975380+logankim0212@users.noreply.github.com>
2018-12-14 17:07:32 -0500

    Add project files.

---
+++ COMP229_FinalProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP229_FinalProject.MvcApplication" Language="C#" %>
---
+++ COMP229_FinalProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP229_FinalProject.Startup))]
+namespace COMP229_FinalProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
